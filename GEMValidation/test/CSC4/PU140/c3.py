83ac990ab3293043811c3bc29d99094d13e616cd
Jose Dimas <jrdv009@neo.tamu.edu>
2014-06-05 04:03:57 -0500

    First Comit

---
+++ GEMValidation/test/CSC4/PU140/c3.py
@@ -0,0 +1,97 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("GEMCSCANA")
+
+## Standard sequence
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.Geometry.GeometryExtended2023Muon_cff')
+process.load('Configuration.Geometry.GeometryExtended2023MuonReco_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+## TrackingComponentsRecord required for matchers
+process.load('TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorOpposite_cfi')
+process.load('TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAlong_cfi')
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:out_L1.root')
+)
+
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
+
+## input
+from GEMCode.SimMuL1.GEMCSCTriggerSamplesLib import *
+from GEMCode.GEMValidation.InputFileHelpers import *
+#process = useInputDir(process, files['_gem98_pt2-50_PU0_pt0_new'], False)
+
+suffix = 'PU140_alctGhostCancellationSideQuality'
+process = useInputDir(process, eosfiles[suffix], True)
+
+#output
+#process.TFileService = cms.Service("TFileService",
+#    fileName = cms.string("gem-csc_stub_ana.root")
+#)
+
+
+outputFileName = 'CSC4' + "_" + suffix + '_eff.root'
+process.TFileService = cms.Service("TFileService",
+     fileName = cms.string(outputFileName)
+)
+
+
+## global tag for upgrade studies
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgradePLS3', '')
+
+# the analyzer configuration
+def enum(*sequential, **named):
+  enums = dict(zip(sequential, range(len(sequential))), **named)
+  return type('Enum', (), enums)
+Stations = enum('ALL','ME11','ME1a','ME1b','ME12','ME13','ME21','ME22','ME31','ME32','ME41','ME42')
+
+from GEMCode.GEMValidation.simTrackMatching_cfi import SimTrackMatching
+process.GEMCSCAnalyzer = cms.EDAnalyzer("GEMCSCAnalyzer",
+    verbose = cms.untracked.int32(0),
+    stationsToUse = cms.vint32(Stations.ME11,Stations.ME1a,Stations.ME1b,
+                               Stations.ME21,Stations.ME31,Stations.ME41),
+    simTrackMatching = SimTrackMatching
+)
+matching = process.GEMCSCAnalyzer.simTrackMatching
+matching.simTrack.minPt = 1.5
+matching.gemRecHit.input = ""
+"""
+matching.cscTfTrack.input = ""
+matching.tfCand.input = ""
+matching.gmtCand.input = ""
+matching.l1Extra.input = ""
+"""
+doGem = True
+if doGem:
+  matching.cscSimHit.minNHitsChamber = 3
+  matching.cscStripDigi.minNHitsChamber = 3
+  matching.cscWireDigi.minNHitsChamber = 3
+  matching.cscCLCT.minNHitsChamber = 3
+  matching.cscALCT.minNHitsChamber = 3
+  matching.cscLCT.minNHitsChamber = 3
+  matching.cscLCT.matchAlctGem = True
+  matching.cscMPLCT.minNHitsChamber = 3
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.p = cms.Path(process.GEMCSCAnalyzer)
+
+## messages
+print
+print 'Input files:'
+print '----------------------------------------'
+print process.source.fileNames
+print
+print 'Output file:'
+print '----------------------------------------'
+print process.TFileService.fileName
+print
